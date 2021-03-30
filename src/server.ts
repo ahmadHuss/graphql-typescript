b4a6632604750858850166712178645a2096a8a1
ahmadhuss <ahmadnet600@gmail.com>
2021-03-30 12:55:38 +0500

    refactor: configure server.ts * pass express app to apollo server middleware

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,16 @@
-console.log('Hello world!');
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import cors from 'cors';
+import schema  from './schema';
+
+const app = express();
+
+const server = new ApolloServer({schema, playground: true});
+
+app.use('*', cors());
+
+server.applyMiddleware({app, path: '/graphql'});
+
+app.listen({port: 8000}, () => {
+    console.log('ApolloServer is running on http://localhost:8000/graphql')
+});
